fc67bebbae3630bb872f127892cc7ef25c63df38
Casper Sparre <reptail@gmail.com>
2023-11-20 18:24:13 +0100

    Added DTO project from API repository

---
+++ CentricaBeerExchange.Dto/Auth/AccessToken.cs
@@ -0,0 +1,21 @@
+﻿namespace CentricaBeerExchange.Dto.Auth;
+
+[DataContract]
+public class AccessToken
+{
+    public AccessToken()
+    {
+        Token = string.Empty;
+    }
+
+    public AccessToken(string token, DateTime expiresAtUtc)
+    {
+        Token = token;
+        ExpiresAtUtc = expiresAtUtc;
+    }
+
+    [DataMember(Order = 1)] public string Token { get; set; }
+    [DataMember(Order = 2)] public DateTime ExpiresAtUtc { get; set; }
+
+    internal static AccessToken Empty => new();
+}
---
+++ CentricaBeerExchange.Dto/Auth/AccessTokenResponse.cs
@@ -0,0 +1,17 @@
+namespace CentricaBeerExchange.Dto.Auth;
+
+[DataContract]
+public class TokenResponse
+{
+    public TokenResponse()
+        : this(AccessToken.Empty, AccessToken.Empty) { }
+
+    public TokenResponse(AccessToken accessToken, AccessToken refreshToken)
+    {
+        AccessToken = accessToken;
+        RefreshToken = refreshToken;
+    }
+
+    [DataMember(Order = 1)] public AccessToken AccessToken { get; set; }
+    [DataMember(Order = 2)] public AccessToken RefreshToken { get; set; }
+}
