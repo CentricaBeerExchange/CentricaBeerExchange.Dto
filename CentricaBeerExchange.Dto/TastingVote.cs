289536526a43fc6360a8c8badf84aaec030bda68
Casper Sparre <reptail@gmail.com>
2024-02-29 21:40:26 +0100

    Added TastingVote models

---
+++ CentricaBeerExchange.Dto/TastingVote.cs
@@ -0,0 +1,24 @@
+﻿namespace CentricaBeerExchange.Dto;
+
+[DataContract]
+public class TastingVote
+{
+    public TastingVote()
+    {
+        UserName = string.Empty;
+        VotedUserName = string.Empty;
+    }
+
+    public TastingVote(int userId, string userName, int votedUserId, string votedUserName)
+    {
+        UserId = userId;
+        UserName = userName;
+        VotedUserId = votedUserId;
+        VotedUserName = votedUserName;
+    }
+
+    [DataMember(Order = 1)] public int UserId { get; set; }
+    [DataMember(Order = 2)] public string UserName { get; set; }
+    [DataMember(Order = 3)] public int VotedUserId { get; set; }
+    [DataMember(Order = 4)] public string VotedUserName { get; set; }
+}
---
+++ CentricaBeerExchange.Dto/TastingVoteRegistration.cs
@@ -0,0 +1,16 @@
+﻿namespace CentricaBeerExchange.Dto;
+
+[DataContract]
+public class TastingVoteRegistration
+{
+    public TastingVoteRegistration() { }
+
+    public TastingVoteRegistration(int userId, int votedUserId)
+    {
+        UserId = userId;
+        VotedUserId = votedUserId;
+    }
+
+    [DataMember(Order = 1)] public int UserId { get; set; }
+    [DataMember(Order = 2)] public int VotedUserId { get; set; }
+}
